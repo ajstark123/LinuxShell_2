62861bd868ccd15a5b3c02b3ad00b0e79057f989
ajstark123 <albert@ajstark.org>
2016-12-05 19:25:35 -0500

    committing the compile.sh

---
+++ bin/run_poj_test_driver.sh
@@ -0,0 +1,27 @@
+
+#
+#
+# runs the poj test driver
+#
+#
+
+BUILD_HOME=/Users/Albert/Documents/Development/build/LinuxShellBuild
+
+cd $BUILD_HOME
+pwd
+echo " "
+echo " "
+
+export CLASSES_TEST_DRIVERS=$BUILD_HOME/out_test_drivers
+
+export LIB_HOME=$BUILD_HOME/lib
+
+export BIN_HOME=$BUILD_HOME/bin
+
+echo " "
+echo " "
+java -version
+
+
+java -classpath $LIB_HOME/linux_shell.jar;$CLASSES_TEST_DRIVERS  org.ajstark.LinixShell.TestDriver.TestDrive
+
